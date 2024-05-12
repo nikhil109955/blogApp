8bb17795210239697c094fbc3f50cd8e9791564c
Nikhil Kumar <ny9650@gmail.com>
2024-05-12 17:07:54 -0400

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,9 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
+import pkg from 'jsonwebtoken';
+
+const jwt = pkg;
 
 export const signup = async (req, res, next) =>{
     
@@ -31,4 +34,34 @@ export const signup = async (req, res, next) =>{
     }
 
 
+};
+
+export const signin  = async (req, res, next) => {
+     
+    const {email, password } =req.body;
+
+    if(!email || !password || email === '' || password === ''){
+        next(errorHandler(400, 'All Fields are required'));
+    }
+
+    try{
+        const validUser = await User.findOne({email});
+        if(!validUser){
+            next(errorHandler(404, 'User not found'))
+        }
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if(!validPassword){
+            next(errorHandler(404, 'Invalid Password'))
+        }
+
+        const token  = jwt.sign(
+           { id: validUser._id}, process.env.JWT_SECRET
+        )
+
+        res.status(200).cookie('access_token', token,{
+            httpOnly: true}).json(validUser);
+        }
+    catch(error){
+        next(error);
+    }
 }
