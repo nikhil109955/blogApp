e48851f08271f2adcd0bcc9696410db8a6d9748c
Nikhil Kumar <ny9650@gmail.com>
2024-06-01 11:01:53 -0400

    complete profile page UI

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,21 +1,22 @@
-import { useEffect,useState } from "react"
-import { useLocation } from "react-router-dom"
-import DashSidebar from "../components/DashSidebar"
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import DashSidebar from "../components/DashSidebar";
+import DashProfile from "../components/DashProfile"; // Import DashProfile component
 
 export default function Dashboard() {
+  const location = useLocation();
+  const [tab, setTab] = useState('');
 
-  const location = useLocation()
-  const[tab, setTab] = useState('')
-  useEffect(()=>{
-    const urlParams = new URLSearchParams(location.search)
-    const tabFromUrl = urlParams.get('tab')
-    console.log(tabFromUrl)
-  },[location.search])
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get('tab');
+    setTab(tabFromUrl); // Update tab state with tabFromUrl
+  }, [location.search]);
+
   return (
     <div className="min-h-screen flex flex-col md:flex-row">
-      <div className="md: w-56"><DashSidebar/>
-      </div>
-      {tab === 'profile' && <DashProfile/>}
-      </div>
-  )
+      <div className="md:w-56"><DashSidebar/></div>
+      {tab === 'profile' && <DashProfile/>} {/* Conditionally render DashProfile */}
+    </div>
+  );
 }
