9086ffa7c895d8b264e141cdb93c7d379ae444b0
Nikhil Kumar <ny9650@gmail.com>
2024-06-08 19:53:49 -0400

    sign out functionality added

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -72,3 +72,13 @@ export const deleteUser = async (req, res, next) =>{
         next(error)
     }
 }
+
+export const signout = (req, res, next) =>{
+    try{
+        res.clearCookie('access_token').status(200).json('user has been sign out');
+    }
+    catch(error){
+        next(error)
+        
+    }
+}
