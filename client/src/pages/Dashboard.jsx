3eaa5e912bff60463a720e0d111a894b06605001
Nikhil Kumar <ny9650@gmail.com>
2024-06-16 19:11:20 -0400

    add dashposts on dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import DashSidebar from "../components/DashSidebar";
 import DashProfile from "../components/DashProfile"; // Import DashProfile component
+import DashPosts from "../components/DashPosts";
 
 export default function Dashboard() {
   const location = useLocation();
@@ -17,6 +18,7 @@ export default function Dashboard() {
     <div className="min-h-screen flex flex-col md:flex-row">
       <div className="md:w-56"><DashSidebar/></div>
       {tab === 'profile' && <DashProfile/>} {/* Conditionally render DashProfile */}
+      {tab === 'posts' && <DashPosts/>}
     </div>
   );
 }
